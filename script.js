c3587be0b029b48686613428cdd10130316f0b16
ElektroLudwig <vmendel88ps4@gmail.com>
2026-09-15 12:43:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+/* ==================================================
+   PRI OBNOVENÍ STRÁNKY VŽDY ZAČAŤ HORE
+================================================== */
+
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+window.addEventListener("load", function() {
+
+  if (!window.location.hash) {
+    window.scrollTo(0, 0);
+  }
+
+});
+
+
+/* ==================================================
+   ANIMÁCIE PRI SCROLLOVANÍ
+================================================== */
+
 const revealElements =
   document.querySelectorAll(".reveal");
 
